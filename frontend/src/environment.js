b277c7d3dd717e321e46340ab64aac4e592bb85b
Abhishek8208 <165295712+Abhishek8208@users.noreply.github.com>
2025-07-23 12:08:59 +0530

    Update environment.js
    updated

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://connectobackend-n52v.onrender.com" :
+    "https://connectovideocallbackend.onrender.com" :
 
     "http://localhost:8000"
 
